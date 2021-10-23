298dca0774d1bcbea0c8f1022ff282d3d170bb5f
Oleh Lavro <oleglavro6@gmail.com>
2021-10-23 16:26:44 +0300

    healthbar

--- Assets/scrips/bossBehaving.cs
+++ Assets/scrips/bossBehaving.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class bossBehaving : MonoBehaviour
 {
-    [SerializeField] private int health;
+    [SerializeField] private float MaxHealth;
+    private float health;
     [SerializeField] private int damage;
     private Rigidbody2D rb;
     private Animator anim;
     [SerializeField]private float moveSpeed;
     [SerializeField]private float frequency;
     [SerializeField]private float magnitude;
+
+    [SerializeField] private Slider healthBar;
 
     bool facingRigt = true;
 
@@ -19,13 +23,27 @@ public class bossBehaving : MonoBehaviour
     [SerializeField] Transform limitePoint, limitePoint2;
     private void Start()
     {
+        health = MaxHealth;
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         pos = transform.position;
         localScale = transform.localScale;
+        healthBar.value = CalculateHealth();
     }
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            health -= 10; 
+            healthBar.value = CalculateHealth();
+        }
+        if (health <= 0)
+        {
+            Debug.Log("Die");
+        }
+
+
+
         CheckWhereToFace();
 
         if (facingRigt)
@@ -57,5 +75,20 @@ public class bossBehaving : MonoBehaviour
     {
         pos -= transform.right * Time.deltaTime * moveSpeed;
         transform.position = pos + transform.up * Mathf.Sin(Time.time * frequency) * magnitude;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+           //this.health -= collision.gameObject.damage;
+        }
+    }
+
+    float CalculateHealth()
+    {
+        print(health / MaxHealth);
+        return health / MaxHealth; 
     }
 }
