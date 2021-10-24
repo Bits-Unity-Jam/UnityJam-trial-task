e1d928995c3078c98505e8b8cbd154058c330d36
Bohdan_Shupiak <darkmesnuk@gmail.com>
2021-10-24 12:06:14 +0300

    Realese: Beta 0.1

---
+++ Assets/Profect Info.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfectInfo : MonoBehaviour
+{
+    /* Project beta 0.1
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+}
